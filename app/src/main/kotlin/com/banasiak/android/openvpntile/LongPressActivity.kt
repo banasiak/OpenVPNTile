d42b398f971bcf6e98567f09d6b76d494c5bfdbc
Richard Banasiak <richard@banasiak.com>
2019-01-06 14:12:15 -0600

    Added ability to launch OpenVPN Connect by long pressing the tile

---
+++ app/src/main/kotlin/com/banasiak/android/openvpntile/LongPressActivity.kt
@@ -0,0 +1,22 @@
+package com.banasiak.android.openvpntile
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LongPressActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    var intent = packageManager.getLaunchIntentForPackage(QuickSettingsService.OPENVPN_PACKAGE)
+    if (intent == null) {
+      intent = Intent(Intent.ACTION_VIEW)
+      intent.data = Uri.parse("market://details?id=${QuickSettingsService.OPENVPN_PACKAGE}")
+    }
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    startActivity(intent)
+    finish()
+  }
+
+}
